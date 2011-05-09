d13af4c35b5e4c0313e54f184a568ce91dedeb31
Vibin Nair <vibin.nair@gmail.com>
2011-05-09 15:54:51 +0530

    commited first app into git.

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '574cd4f59ef5094567ecc5860c395d5b256e99ed5daf6c712327ae7734ddaebd45858a2ec28dcf848f99b72ebff6b55645464fe84d8cec66bc0bfe962db41581'
